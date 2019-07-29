f5af893009181da302d1b091fcfae6aae3046756
calebredd <caleb.redd@yahoo.com>
2019-07-29 13:29:44 -0700

    Set up project to call API and then stored test data in json file under public/db.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  useEffect(() => {
+    axios
+      //.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      .get("db.json")
+      .then(api => {
+        console.log(api.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
   return (
     <div className="App">
       <p>
