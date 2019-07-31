5bf13386b769a68e40628dbbdf1c1fc1e159e9ce
calebredd <caleb.redd@yahoo.com>
2019-07-30 18:56:59 -0700

    Added button for generating random Photo of the Day from the year 2018. Also displays date being shown above button.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,30 @@ import NasaCard from "./Components/NasaCard";
 
 function App() {
   const today = new Date();
-  const day = today.getFullYear() + '-' + ("0"+(today.getMonth() + 1)).slice(-2) + '-' + ("0"+today.getDate()).slice(-2);
+  const day =
+    today.getFullYear() +
+    "-" +
+    ("0" + (today.getMonth() + 1)).slice(-2) +
+    "-" +
+    ("0" + today.getDate()).slice(-2);
   const [url, setUrl] = useState([]);
   const [title, setTitle] = useState([]);
   const [explanation, setExplanation] = useState([]);
-  const [date, setDate] = useState([{day}]);
-
+  const [date, setDate] = useState(day);
+  const randomDate = () => {
+    return (
+      "2018" +
+      "-" +
+      ("0" + (Math.floor(Math.random() * Math.floor(12)) + 1)).slice(-2) +
+      "-" +
+      ("0" + Math.floor(Math.random() * Math.floor(28))).slice(-2)
+    );
+  };
+  console.log({date});
   useEffect(() => {
     axios
-      // .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date={date}")
-      .get("db.json")
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date="+date)
+      // .get("db.json")
       .then(api => {
         console.log(api.data);
         setTitle(api.data.title);
@@ -22,9 +36,18 @@ function App() {
         return setUrl(api.data.url);
       })
       .catch(err => console.log(err));
-  }, []);
+  }, [date]);
   return (
     <div className="App">
+      <div className="displaying-date">Current date being displayed: {date}</div>
+      <button
+        className="random-btn"
+        onClick={() => {
+          return setDate(randomDate());
+        }}
+      >
+        Random Picture
+      </button>
       <NasaCard title={title} url={url} explanation={explanation} />
     </div>
   );
