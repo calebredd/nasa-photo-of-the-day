fece7da41ee050753a61eae8c70a66e0c65907db
calebredd <caleb.redd@yahoo.com>
2019-07-31 20:03:43 -0700

    Added Semantic features with Button, Card, Container, Icon  and Image components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import NasaCard from "./Components/NasaCard";
-import {Grid, Form, Input, TextArea, Button, Select} from "semantic-ui-react";
+// import NasaCard from "./Components/NasaCard";
+import {
+  Container,
+  Button,
+  Card,
+  Image,
+  Icon
+} from "semantic-ui-react";
 
 import "./App.css";
-import "semantic-ui/dist/semantic.min.css";
+//import "semantic-ui-react/semantic.min.css";
 
 function App() {
   const today = new Date();
@@ -17,44 +23,73 @@ function App() {
   const [url, setUrl] = useState([]);
   const [title, setTitle] = useState([]);
   const [explanation, setExplanation] = useState([]);
+  const [copyright, setCopyright] = useState([]);
   const [date, setDate] = useState(day);
   const randomDate = () => {
     return (
-      today.getFullYear()-(Math.floor(Math.random() * Math.floor(10))) +
+      today.getFullYear() -
+      Math.floor(Math.random() * Math.floor(10)) +
       "-" +
       ("0" + (Math.floor(Math.random() * Math.floor(12)) + 1)).slice(-2) +
       "-" +
-      ("0" + Math.floor(Math.random() * Math.floor(28))).slice(-2)
+      ("0" + (Math.floor(Math.random() * Math.floor(28))+1)).slice(-2)
     );
   };
   // console.log({date});
   useEffect(() => {
     axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date="+date)
+      .get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=" + date)
       // .get("db.json")
       .then(api => {
         // console.log(api.data);
         setTitle(api.data.title);
         setExplanation(api.data.explanation);
         setUrl(api.data.url);
+        setCopyright(api.data.copyright);
         if (api.data.url.slice(-3) !== "jpg") return setDate(randomDate());
         return;
       })
       .catch(err => console.log(err));
-    }, [date]);
-    return (
-      <div className="App">
-      <div className="displaying-date">Current date being displayed: {date}</div>
-      <button
-        className="random-btn"
-        onClick={() => {
-          return setDate(randomDate());
-        }}
-        >
-        Random Picture
-      </button>
-      <NasaCard title={title} url={url} explanation={explanation} setDate={setDate} randomDate={randomDate}  />
-    </div>
+  }, [date]);
+  return (
+    <Container className="App" fluid={true}>
+      <Card className="display-card" centered={true}>
+        <Card.Header className="displaying-date">
+          Current date being displayed: {date}
+        </Card.Header>
+        <Card.Content>
+          <Button
+          primary inverted
+            // className="random-btn"
+            onClick={() => {
+              return setDate(randomDate());
+            }}
+          >
+            {" "}
+            Random Picture
+          </Button>
+        </Card.Content>
+      </Card>
+
+      {/*    <NasaCard title={title} url={url} explanation={explanation} setDate={setDate} randomDate={randomDate}  /> */}
+
+      <Container className="main" centered={true}>
+        <Card fluid>
+          <Card.Content>
+          <Card.Header>{title}</Card.Header>
+          <Card.Meta>
+            <span className="date">{date}</span>
+          </Card.Meta>
+        <Image src={url} wrapped ui={false} />
+          <Card.Description>{explanation}</Card.Description>
+        </Card.Content>
+        <Card.Content extra textAlign="left">
+          <Icon disabled name='copyright' />
+          {copyright}
+        </Card.Content>
+      </Card>
+      </Container>
+    </Container>
   );
 }
 
